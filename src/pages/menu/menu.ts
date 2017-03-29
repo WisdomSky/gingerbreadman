ee5a324d8f4ade5f590d326f02fba08956fd0fb8
WisdomSky <i.wisdomsky@gmail.com>
2017-03-30 01:05:33 +0800

    refactor menu

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import stories from './../stories/index';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-menu',
@@ -8,27 +9,75 @@ import { NavController } from 'ionic-angular';
 })
 export class MenuPage {
 
-  private status = 'login';
+  private status = 'main';
 
 
-  private login_username = '';
-  private login_password = '';
-
-  private user_has_saved_story = true;
-
-  private stories_list = {};
-  private stories_titles = [];
+  private login_name          = '';
+  private new_user_name       = '';
+  private new_user_error      = '';
+  private stories_list        = {};
+  private stories_titles      = [];
+  private users_list          = [];
+  private local               = new Storage();
+  private delete_user_name    = '';
 
   constructor(public navCtrl: NavController) {
-      this.stories_list = stories;
-      this.stories_titles = Object.keys(this.stories_list);
+    this.stories_list   = stories;
+    this.stories_titles = Object.keys(this.stories_list);
+
+    this.local.get('user').then((res) => {
+      if (res != null) {
+        this.users_list = JSON.parse(res);
+      }
+    });
 
   }
 
+  addNewUser() {
 
-  public doLogin()
-  {
-    this.status = 'menu';
+    let name = this.new_user_name.trim();
+
+    if (name.length == 0) {
+      this.new_user_error = 'Please enter a name.';
+      return;
+    }
+
+    if (this.users_list.indexOf(name) != -1) {
+      this.new_user_error = 'Name is already used.';
+      return;
+    }
+
+    this.users_list.push(name);
+    this.new_user_name = this.new_user_error = '';
+    this.local.set('user', JSON.stringify(this.users_list));
+    this.loginUser(name);
   }
+
+
+  confirmRemoveUser(name) {
+
+    this.delete_user_name = name;
+    this.status = 'delete_user_confirm';
+
+  }
+  removeUser() {
+
+    let index = this.users_list.indexOf(this.delete_user_name);
+    this.users_list.splice(index, 1);
+    this.local.set('user', JSON.stringify(this.users_list));
+    this.delete_user_name = '';
+    if (!this.users_list.length) {
+      this.status = 'main';
+      return;
+    }
+    this.status = 'delete_user';
+  }
+
+
+  loginUser(name) {
+    this.login_name = name;
+    this.status = 'stories_list';
+  }
+
 
 }
